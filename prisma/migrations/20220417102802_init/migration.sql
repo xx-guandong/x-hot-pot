58e9c32113a1a4f242740fed6cf39755a745e1c1
An dong Xiao <andongxi14@gmail.com>
2022-04-17 18:30:27 +0800

    postgresql

---
+++ prisma/migrations/20220417102802_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Page" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "viewCount" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Page_pkey" PRIMARY KEY ("id")
+);
