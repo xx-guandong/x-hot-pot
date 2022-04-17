60fc51a44c51a65ce363509f2345ae05e72b4e91
An dong Xiao <andongxi14@gmail.com>
2022-04-17 20:42:08 +0800

    postgresql

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { Page } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../lib/prisma";
+import { WithData } from "./typings";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<WithData<Page[]>>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  // const PrismaClient
+  const data = await prisma.page.findMany();
+  res.status(200).json({ data });
 }
---
+++ pages/api/typings.ts
@@ -0,0 +1,3 @@
+export interface WithData<T> {
+  data: T;
+}
