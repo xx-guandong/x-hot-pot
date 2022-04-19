8f1b89d6c1234a6672a5064a0c06bead5a977577
An dong Xiao <andongxi14@gmail.com>
2022-04-19 09:03:04 +0800

    add tsconfig

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import SideNavBar from "../components/layout/SideNavBar/default";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="flex flex-col min-h-full">
+      {/* <HeaderNavBar /> */}
+      <div className="flex">
+        <SideNavBar />
+        <Component className="flex-auto" {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
